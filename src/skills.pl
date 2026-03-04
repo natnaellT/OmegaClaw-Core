fb96d37c3f097d0ed5f89ecc46fe48181a974415
Patrick Hammer <patham9@gmail.com>
2026-03-04 04:55:39 +0100

    Update: reminder that steers against strange safety attractors

--- src/skills.pl
+++ src/skills.pl
@@ -7,3 +7,5 @@ shell(Cmd, Out) :- format(string(SafeCmd), "timeout -k 1s 10s sh -c '~w'", [Cmd]
                    process_wait(P, Status),
                    ( Status = exit(124) -> Out = timeout_error
                                          ; Out = Text ).
+
+first_char(Str, C) :- sub_string(Str, 0, 1, _, C).
